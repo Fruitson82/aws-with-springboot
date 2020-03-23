8cf5fd1fce245d9651ed750f0f1f833e682a3e3a
Fruitson <fruitson@nomadconnection.com>
2020-03-23 17:50:12 +0900

    02. 스프링 부트에서 테스트 코드를 작성하자  - 테스트 코드 작성
    03. 스프링 부트에서 JPA로 데이터베이스 다뤄보자
     - JPA 적용(Create, Read, Update)
     - JPA 테스트 코드 작성

---
+++ src/main/java/com/fruitson/aws/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package com.fruitson.aws.web;
+
+import com.fruitson.aws.service.PostsService;
+import com.fruitson.aws.web.dto.PostsResponseDto;
+import com.fruitson.aws.web.dto.PostsSaveRequestDto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+}
---
+++ src/main/java/com/fruitson/aws/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,23 @@
+package com.fruitson.aws.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.PostUpdate;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+
+    private String title;
+    private  String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+
+}
