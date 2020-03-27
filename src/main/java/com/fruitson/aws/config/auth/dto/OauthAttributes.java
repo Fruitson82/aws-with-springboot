e43c0218990797870759ae280777cb4ed0f873b0
Fruitson <fruitson@nomadconnection.com>
2020-03-27 17:44:27 +0900

    05. 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기

---
+++ src/main/java/com/fruitson/aws/config/auth/dto/OauthAttributes.java
@@ -0,0 +1,66 @@
+package com.fruitson.aws.config.auth.dto;
+
+import com.fruitson.aws.domain.user.Role;
+import com.fruitson.aws.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OauthAttributes {
+
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OauthAttributes(Map<String, Object> attributes, String nameAttribueKey, String name, String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttribueKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public static OauthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OauthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OauthAttributes.builder()
+                                .name((String) attributes.get("name"))
+                                .email((String) attributes.get("email"))
+                                .picture((String) attributes.get("picture"))
+                                .attributes(attributes)
+                                .nameAttribueKey(userNameAttributeName)
+                                .build();
+    }
+
+    private static OauthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OauthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
+                .nameAttribueKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                    .name(name)
+                    .email(email)
+                    .picture(picture)
+                    .role(Role.GUEST)
+                    .build();
+    }
+}
--- src/main/java/com/fruitson/aws/web/IndexController.java
+++ src/main/java/com/fruitson/aws/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.fruitson.aws.web;
 
+import com.fruitson.aws.config.auth.LoginUser;
+import com.fruitson.aws.config.auth.dto.SessionUser;
 import com.fruitson.aws.service.PostsService;
 import com.fruitson.aws.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +11,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
